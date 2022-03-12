ff8128a376a656c729993750c67f4a62751d7e1d
KananeloPhakisi <phakisikananelo0@gmail.com>
2022-03-12 00:18:16 +0000

    Add user page

--- admin/src/Pages/UserList.jsx
+++ admin/src/Pages/UserList.jsx
@@ -1,13 +1,92 @@
 import styled from 'styled-components';
+import { DataGrid } from '@mui/x-data-grid';
+import { Delete } from '@material-ui/icons';
+import { userRows } from '../data';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 const Container = styled.div`
   flex: 4;
 `;
 
+const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Image = styled.img`
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-right: 10px;
+`;
+
+const EditButton = styled.button`
+  border: none;
+  border-radius: 10px;
+  padding: 5px 10px;
+  background-color: #52b788;
+  color: white;
+  cursor: pointer;
+  margin-right: 20px;
+`;
+
 const UserList = () => {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'user', headerName: 'User', width: 210, renderCell: (params) => {
+      return (
+        <UserContainer>
+          <Image src={params.row.avatar} alt="" />
+          {params.row.username}
+        </UserContainer>
+      )
+    } },
+    { field: 'email', headerName: 'Email', width: 180 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      width: 100,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction Value',
+      width: 140,
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={'/user/'+params.row.id}>
+              <EditButton>Edit</EditButton>
+            </Link>
+            <Delete style={{ color: '#d90429', cursor: 'pointer' }} onClick={() => handleDelete(params.row.id)}/>
+          </>
+        )
+      }
+    },
+  ];
+
   return (
     <Container>
-      UserList
+      <DataGrid
+        rows={data}
+        disableSelectionOnClick
+        columns={columns}
+        pageSize={10}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
     </Container>
   )
 }
--- admin/src/data.js
+++ admin/src/data.js
@@ -48,3 +48,62 @@ export const userData  = [
     "Active Users": 3590,
   },
 ];
+
+export const userRows = [
+  { 
+    id: 1, 
+    username: 'jon Snow',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'jonsnow@gmail.com',
+    status: 'active',
+    transaction: 'M1000',
+  },
+  { 
+    id: 2, 
+    username: 'Samuel Tally',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'samueltally@gmail.com',
+    status: 'active',
+    transaction: 'M2000',
+  },
+  { 
+    id: 3, 
+    username: 'Jack Sparrow',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'jacksparrow@gmail.com',
+    status: 'active',
+    transaction: 'M100',
+  },
+  { 
+    id: 4, 
+    username: 'John Smith',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'johnsmith@gmail.com',
+    status: 'active',
+    transaction: 'M20',
+  },
+  { 
+    id: 5, 
+    username: 'Joe',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'joe@gmail.com',
+    status: 'active',
+    transaction: 'M10000',
+  },
+  { 
+    id: 6, 
+    username: 'johnny',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'johnny@gmail.com',
+    status: 'active',
+    transaction: 'M200',
+  },
+  { 
+    id: 7, 
+    username: 'Jane',
+    avatar: "https://i.ibb.co/r7hyySM/cda8ff1bcb7f335719b146d61f6f494a.png",
+    email: 'jane@gmail.com',
+    status: 'active',
+    transaction: 'M5000',
+  },
+];
