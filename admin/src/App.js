814f5f123b1edb8806e1504b372a4fd0586e6128
KananeloPhakisi <phakisikananelo0@gmail.com>
2022-05-03 01:30:13 +0200

    create private routes

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Topbar from "./Components/Topbar";
@@ -22,33 +23,140 @@ import TransactionDetail from './Pages/TransactionDetail';
 import Analytics from './Pages/Analytics';
 import Login from './Pages/Login';
 
+
+const Container = styled.div`
+display: flex;
+margin-top: 10px;
+`;
+
 function App() {
   const admin = useSelector((state) => state.user.currentUser);
+  
+  function PrivateRoute({ admin, children }) {
+    return admin ? children : <Navigate to="/login" />;
+  }
+
   return (
     <Router>
         <Routes>
-          <Route path='/login' element={ <Login/> } />
-          {admin && (
-            <>
+        <Route path='/login' index element={ <Login/> } />
+          <Route path='/' element={
+            <PrivateRoute admin={admin}>
               <Topbar />
-              <div className='container'>
+              <Container>
                 <Sidebar />
-                <Route path='/' element={<Home/>} />
-                <Route path='/users' element={<UserList/>} />
-                <Route path='/user/:userId' element={<User/>} />
-                <Route path='/newUser' element={<NewUser />} />
-                <Route path='/products' element={<ProductList/>} />
-                <Route path='/product/:productId' element={<Product />} />
-                <Route path='/newProduct' element={<NewProduct />} />
-                <Route path='/staff' element={<StaffList/>} />
-                <Route path='/staff/:userId' element={<Staff/>} />
-                <Route path='/newStaff' element={<NewStaff />} />
-                <Route path='/transactions' element={<Transactions />} />
-                <Route path='/transactions/:transId' element={<TransactionDetail />} />
-                <Route path='/analytics' element={<Analytics />} />
-              </div>
-            </>
-          )}
+                <Home/>
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/users' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <UserList/>
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/user/:userId' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <User/>
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/newUser' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <NewUser />
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/products' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <ProductList/>
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/product/:productId' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <Product />
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/newProduct' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <NewProduct />
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/staff' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <StaffList/>
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/staff/:userId' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <Staff/>
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/newStaff' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <NewStaff />
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/transactions' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <Transactions />
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/transactions/:transId' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <TransactionDetail />
+              </Container>
+            </PrivateRoute>
+          }/>
+          <Route path='/analytics' element={
+            <PrivateRoute admin={admin}>
+              <Topbar />
+              <Container>
+                <Sidebar />
+                <Analytics />
+              </Container>
+            </PrivateRoute>
+          }/>
         </Routes>
     </Router>
   );
